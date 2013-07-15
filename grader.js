82cacf80683fc02c459e7c1385c69bc3f685a8bd
Harivansh Kumar <vansh8503@gmail.com>
2013-07-15 13:04:59 +0000

    Modified grader.js to take input from url, added more checks to checks.json

--- grader.js
+++ grader.js
@@ -20,6 +20,7 @@ References:
 */
 
 var fs = require('fs');
+var rest = require('restler');
 var program = require('commander');
 var cheerio = require('cheerio');
 var HTMLFILE_DEFAULT = "index.html";
@@ -59,13 +60,41 @@ var clone = function(fn) {
     return fn.bind({});
 };
 
+var buildfn = function(htmlfile) {
+    var response2console = function(result, response) {
+	if(result instanceof Error) {
+console.error('Error: ' + util.format(response.message));
+	} else {
+console.error("Wrote %s", htmlfile);
+fs.writeFileSync(htmlfile, result);
+	}
+    };
+    return response2console;
+};
+
+/*var processInput = function(htmlfile, checksJsonfile) {
+    htmlfile = htmlfile || HTMLFILE_DEFAULT;
+    checksJsonFile = checksJsonFile || CHECKSFILE_DEFAULT;
+
+    var response2console = buildfn(htmlfile);
+    rest.get(
+};*/
+
 if(require.main == module) {
     program
 	.option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
 	.option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+	.option('-u, --url <app_url>', 'URL to hosted website', null, null)
 	.parse(process.argv);
 
-    var checkJson = checkHtmlFile(program.file, program.checks);
+    var htmlfile = program.file || HTMLFILE_DEFAULT;
+
+    if(program.url !== null) {
+	var response2console = buildfn(htmlfile);
+	rest.get(program.url).on('complete', response2console);
+    }
+
+    var checkJson = checkHtmlFile(htmlfile, program.checks);
     var outJson = JSON.stringify(checkJson, null, 4);
     console.log(outJson);
 } else {
